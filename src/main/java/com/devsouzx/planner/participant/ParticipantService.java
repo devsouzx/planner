da134c9d6de346e3655fcb28ac20d6d3597fee70
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-07-09 18:04:59 -0300

    Aula 1

---
+++ src/main/java/com/devsouzx/planner/participant/ParticipantService.java
@@ -0,0 +1,15 @@
+package com.devsouzx.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+    public void registerParticipantsToEvents(List<String> participantsToInvite, UUID tripId) {
+    }
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId) {
+    }
+}
---
+++ src/main/java/com/devsouzx/planner/trip/TripCreateResponse.java
@@ -0,0 +1,6 @@
+package com.devsouzx.planner.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(UUID tripId) {
+}
